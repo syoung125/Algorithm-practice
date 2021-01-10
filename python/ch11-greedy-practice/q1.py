fad89c759e3570ce4558beecd906ca7627e17dc9
syoung125 <laura120555@gmail.com>
2021-01-10 19:45:01 +0900

    fix: handle exception

--- python/ch11-greedy-practice/q1.py
+++ python/ch11-greedy-practice/q1.py
@@ -1,17 +1,19 @@
-# -*- coding: utf-8 -*-
-# 모험가 길드
+# # -*- coding: utf-8 -*-
+# # 모험가 길드
 
 n = int(input())
 adventurerFears = list(map(int, input().split()))
 adventurerFears.sort(reverse=True)
 
-result = 0
-maxValue = 0
+result, maxValue = 0, 0
 
 for adventurerFear in adventurerFears:
     if maxValue == 0:
         result += 1
         maxValue = adventurerFear
     maxValue -= 1
+
+if maxValue > 0:
+    result -= 1
 
 print(result)
