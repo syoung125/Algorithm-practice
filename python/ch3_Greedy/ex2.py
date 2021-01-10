7bc4c53b022a5b486196a2f900aa0ba954d7c880
syoung125 <laura120555@gmail.com>
2021-01-10 17:59:16 +0900

    feat: clear ch3 greedy ex2

--- python/ch3_Greedy/ex2.py
+++ python/ch3_Greedy/ex2.py
@@ -1,22 +1,17 @@
 # -*- coding: utf-8 -*-
-
 # 큰 수의 법칙
 
-# n, m, k = map(int, input().split())
-# data = list(map(int, input().split()))
+# test input
+# n, m, k = 5, 8, 3
+# data = [2, 4, 5, 4, 6]
 
-n, m, k = 5, 8, 3
-data = [2, 4, 5, 4, 6]
+n, m, k = map(int, input().split())  # input은 string이어야 함 ex) "5 8 3"
+data = list(map(int, input().split()))
 
 data.sort(reverse=True)  # 정렬 (reverse True: 내림차순)
 first = data[0]
 second = data[1]
 
-result = 0
-
-if(k >= m):
-    result = first * m
-else:
-    result = (first * k) * (m // k) + second * (m % k)
+result = (first * k) * (m // k) + second * (m % k)
 
 print(result)
