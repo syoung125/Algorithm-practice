41f4ce4ce31f678605e571a75e14839477ed3b93
syoung125 <laura120555@gmail.com>
2021-01-17 15:31:31 +0900

    feat: enhance ch4 ex2

--- python/ch4-implementation/ex2.py
+++ python/ch4-implementation/ex2.py
@@ -5,9 +5,15 @@ h = int(input())
 
 count = 0
 for i in range(h+1):
+    if '3' in str(i):
+        count += 3600
+        continue
     for j in range(60):
+        if '3' in str(j):
+            count += 60
+            continue
         for k in range(60):
-            if '3' in str(i) + str(j) + str(k):
+            if '3' in str(k):
                 count += 1
 
 print(count)
